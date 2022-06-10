5fadfd671f04b96e3634be08ed081a073fdfefb3
Guyo <you@example.com>
2022-06-11 01:22:51 +0300

    feature --add links to home page

--- api_practise/urls.py
+++ api_practise/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api_home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.StudentSer.as_view())
+    path('',include('api_app.urls')),
+    path('api-endpoint', views.StudentSer.as_view(), name='api-endpoint')
 ]
